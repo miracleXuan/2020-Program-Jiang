556d288eb98dab57f080852839d37eb7f4d89bd9
许嘉轩 <63142695+miracleXuan@users.noreply.github.com>
2020-04-18 00:32:23 +0800

    Update

--- Matrix/Matrix/Matrix.cpp
+++ Matrix/Matrix/Matrix.cpp
@@ -1,21 +1,27 @@
-ï»¿#include <iostream>
+#include <iostream>
 #include "Matrix.h"
 
-void test() 
+void test()
 {
-	int a[4][4];
-	inputMartix4x4(a);
+	int a[4][4]={
+		{1,2,3,4},
+	    {2,3,4,5},
+		{3,4,5,6},
+		{4,5,6,7}
+	};
+	//inputMartix4x4(a);
 	printMartix4x4(a);
-	cout << "ä¸Žè‡ªèº«ç›¸åŠ ï¼š" << endl;
+	cout << "Óë×ÔÉíÏà¼Ó£º" << endl;
 	add(a, a);
-	cout << "ä¸Žè‡ªèº«ç›¸å‡ï¼š" << endl;
+	cout << "Óë×ÔÉíÏà¼õ£º" << endl;
 	substract(a, a);
-	cout << "ä¸Žè‡ªèº«ç›¸ä¹˜ï¼š" << endl;
+	cout << "Óë×ÔÉíÏà³Ë£º" << endl;
 	multiple(a, a);
 }
 
 int main()
 {
-    std::cout << "test:\n";
+	std::cout << "test:\n";
 	test();
+
 }
--- Matrix/Matrix/Matrix.h
+++ Matrix/Matrix/Matrix.h
@@ -11,21 +11,21 @@ void printMartix4x4(int m[4][4]) {
 	}
 }
 
-void inputMartix4x4(int m[4][4]) {
-	for (int i = 0; i < 4; ++i) {
-		for (int j = 0; j < 4; ++j) {
-			cin >> m[i][j];
-			cout << " ";
-		}
-		cout << endl;
-	}
-}
+//void inputMartix4x4(int m[4][4]) {
+//	for (int i = 0; i < 4; ++i) {
+//		for (int j = 0; j < 4; ++j) {
+//			cin >> m[i][j];
+//			cout << " ";
+//		}
+//		cout << endl;
+//	}
+//}
 
 void add(int m1[4][4], int m2[4][4]) {
 	int result[4][4];
 	for (int i = 0; i < 4; ++i)
 	{
-		for (int j = 0; j < 4; ++j) 
+		for (int j = 0; j < 4; ++j)
 		{
 			result[i][j] = m1[i][j] + m2[i][j];
 		}
@@ -51,7 +51,11 @@ void multiple(int m1[4][4], int m2[4][4]) {
 	{
 		for (int j = 0; j < 4; ++j)
 		{
-			result[i][j] = m1[i][j] * m2[j][i];
+			result[i][j] = 0;
+			for (int k = 0; k < 4; ++k)
+			{
+				result[i][j] += m1[i][k] * m2[k][j];
+			}
 		}
 	}
 	printMartix4x4(result);
